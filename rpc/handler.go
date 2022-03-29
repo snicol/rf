669b5e733ca36b074dae46f20b7a82c7d179c0a7
Scott Nicol <s.cott@me.com>
2022-03-29 19:27:01 +0100

    remove function wrapping

--- rpc/handler.go
+++ rpc/handler.go
@@ -1,13 +1,87 @@
 package rpc
 
 import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/snicol/rf"
+	"github.com/snicol/yael"
+	"github.com/xeipuuv/gojsonschema"
 )
 
-func (rpc *Handler) Handle() rf.HandlerFunc {
-	return rpc.fn
+func (h *Handler[Req, Res]) Handle() rf.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) error {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			return err
+		}
+
+		if err := validateBody(body, h.schema); err != nil {
+			return err
+		}
+
+		r.Body.Close()
+		r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+
+		var req Req
+		err = json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			return err
+		}
+
+		res, err := h.fn(r.Context(), req)
+		if err != nil {
+			return err
+		}
+
+		if res == *new(Res) {
+			result(w, "", http.StatusNoContent, nil)
+			return nil
+		}
+
+		resp, err := json.Marshal(res)
+		if err != nil {
+			return err
+		}
+
+		result(w, string(resp), http.StatusOK, &defaultContentType)
+		return nil
+	}
+}
+
+func validateBody(body []byte, schema gojsonschema.JSONLoader) error {
+	if schema == nil {
+		return nil
+	}
+
+	bodyLoader := gojsonschema.NewBytesLoader(body)
+	schemaRes, err := gojsonschema.Validate(schema, bodyLoader)
+	if err != nil {
+		return err
+	}
+
+	if schemaRes.Valid() {
+		return nil
+	}
+
+	yErr := yael.New(yael.BadRequest)
+
+	yErr.Meta = map[string]interface{}{
+		"schema_error": make([]map[string]interface{}, len(schemaRes.Errors())),
+	}
+
+	for i, reason := range schemaRes.Errors() {
+		seMeta := yErr.Meta["schema_error"].([]map[string]interface{})
+		seMeta[i] = map[string]interface{}{
+			"description": reason.Description(),
+			"field":       reason.Field(),
+			"type":        reason.Type(),
+		}
+	}
+
+	return yErr
 }
 
 func result(w http.ResponseWriter, body string, statusCode int, contentType *string) {
--- rpc/rpc.go
+++ rpc/rpc.go
@@ -1,14 +1,8 @@
 package rpc
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
-	"io/ioutil"
-	"net/http"
 
-	"github.com/snicol/rf"
-	"github.com/snicol/yael"
 	"github.com/xeipuuv/gojsonschema"
 )
 
@@ -17,88 +11,33 @@ var defaultContentType = "application/json"
 // Handler is an instance of a JSON based, POST only, jsonschema validated
 // handler. This handler type is very opinionated and uses the yael package for
 // handling errors.
-type Handler struct {
-	fn rf.HandlerFunc
+type Handler[Req any, Res comparable] struct {
+	fn     RPCHandlerFunc[Req, Res]
+	schema gojsonschema.JSONLoader
 }
+
+type RPCHandlerFunc[Req any, Res comparable] func(context.Context, Req) (Res, error)
 
 // NewHandler returns a handler instance with the provided handler function and
 // jsonschema to validate the request with.
 // The 'fn' argument provided must be a function with a signature like so:
 //     func Example(ctx context.Context, req RequestType) (ResponseType, error)
 // Any mismatch against the above format will result in a panic
-func NewHandler[Request any, Response comparable](
-	fn func(ctx context.Context, req Request) (Response, error),
+func NewHandler[Req any, Res comparable](
+	fn RPCHandlerFunc[Req, Res],
 	schema gojsonschema.JSONLoader,
-) *Handler {
+) *Handler[Req, Res] {
 	if schema != nil {
-		err := validateSchema(schema)
+		_, err := gojsonschema.NewSchemaLoader().Compile(schema)
 		if err != nil {
 			panic(err)
 		}
 	}
 
-	wrappedFn := func(w http.ResponseWriter, r *http.Request) error {
-		ctx := r.Context()
-
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			return err
-		}
-
-		bodyLoader := gojsonschema.NewBytesLoader(body)
-		schemaRes, err := gojsonschema.Validate(schema, bodyLoader)
-		if err != nil {
-			return err
-		}
-
-		if !schemaRes.Valid() {
-			err := yael.New(yael.BadRequest)
-
-			err.Meta = map[string]interface{}{
-				"schema_error": make([]map[string]interface{}, len(schemaRes.Errors())),
-			}
-
-			for i, reason := range schemaRes.Errors() {
-				seMeta := err.Meta["schema_error"].([]map[string]interface{})
-				seMeta[i] = map[string]interface{}{
-					"description": reason.Description(),
-					"field":       reason.Field(),
-					"type":        reason.Type(),
-				}
-			}
-
-			return err
-		}
-
-		r.Body.Close()
-		r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
-
-		var req Request
-		err = json.NewDecoder(r.Body).Decode(&req)
-		if err != nil {
-			return err
-		}
-
-		res, err := fn(ctx, req)
-		if err != nil {
-			return err
-		}
-
-		if res == *new(Response) {
-			result(w, "", http.StatusNoContent, nil)
-			return nil
-		}
-
-		resp, err := json.Marshal(res)
-		if err != nil {
-			return err
-		}
-
-		result(w, string(resp), http.StatusOK, &defaultContentType)
-		return nil
+	h := &Handler[Req, Res]{
+		fn:     fn,
+		schema: schema,
 	}
 
-	return &Handler{
-		fn:     wrappedFn,
-	}
+	return h
 }
